84f76fe11780e3a25098a33c6e3f5f41f685b609
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-07-27 14:56:18 +0300

    add MongoDB connection setup and implement database module

--- connect.js
+++ connect.js
@@ -1,5 +1,8 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config({ path: "./config.env" });
+
+const uri = process.env.DATABASE_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -7,19 +10,18 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+let database;
+
+module.exports = {
+  connectToserver: () => {
+    database = client.db("musics");
+  },
+  getDb: () => {
+    return database;
+  },
+};
+
+console.log("hi from backend");
